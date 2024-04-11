14edeecdbb4ff2691dedd20ad809efc4010374dc
0xSt1ng3R <88931234+0xSt1ng3R@users.noreply.github.com>
2024-04-11 16:04:11 +0300

    Update bundle.rs

--- crates/rpc/rpc/src/eth/bundle.rs
+++ crates/rpc/rpc/src/eth/bundle.rs
@@ -106,9 +106,15 @@ where
             .eth_api
             .spawn_with_state_at_block(at, move |state| {
                 let coinbase_addr = coinbase.unwrap_or(block_env.coinbase);
+                
                 let coinbase = block_env.coinbase;
                 let basefee = Some(block_env.basefee.to::<u64>());
+
                 let env = EnvWithHandlerCfg::new_with_cfg_env(cfg, block_env, TxEnv::default());
+
+                env.coinbase = coinbase;
+                env.basefee = basefee;
+
                 let db = CacheDB::new(StateProviderDatabase::new(state));
 
                 let initial_coinbase = DatabaseRef::basic_ref(&db, coinbase_addr)?
